381a4d1740cf028a21d0c4b92cc68f3243a4e5e9
poparnassus_linux <177719018+Poparnassus30@users.noreply.github.com>
2025-12-25 02:21:12 +0100

    correction du core_6wd

--- wildthumper_6wd/core/core_6wd.py
+++ wildthumper_6wd/core/core_6wd.py
@@ -2,12 +2,20 @@ class WildthumperCore:
     name = "Wildthumper_6wd"
 
     def on_ready(self, ctx):
-        # appelé quand le système est démarré
-        print("online ✅")
-
         # optionnel: tu peux t’abonner à des events si tu veux
         ctx.bus.on("key.ctrl_q", self.on_quit)
+        self.log(ctx, "info" , "Wildthumper Core is ready.")
 
     def on_quit(self, ctx):
         # appelé par masterstruct quand CTRL+Q est détecté
         ctx.kernel.shutdown()
+
+    def log(self, ctx, level: str, msg: str):
+        logger = (
+            getattr(ctx, "logger", None)
+            or getattr(getattr(ctx, "kernel", None), "logger", None)
+            or getattr(getattr(ctx, "system", None), "logger", None)
+        )
+        if logger:
+            fn = getattr(logger, level, logger.info)
+            fn(f"[{self.name}] {msg}")
